89c3150985f03f8a009b8cf1a4558672c98f7681
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-20 17:48:48 -0300

    feat(repository): make new user

---
+++ src/modules/auth/auth.repository.ts
@@ -0,0 +1,25 @@
+import { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import { profiles } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export class AuthRepository {
+  constructor(private db: BetterSQLite3Database) {}
+
+  async findByUsername(username: string) {
+    return this.db.select().from(profiles).where(eq(profiles.username, username)).get();
+  }
+
+  async findById(id: string) {
+    return this.db.select().from(profiles).where(eq(profiles.id, id)).get();
+  }
+
+  async createUser(id: string, username: string, hashedPassword: string) {
+    this.db.insert(profiles).values({
+      id,
+      username,
+      password: hashedPassword,
+    }).run();
+
+    return this.findById(id);
+  }
+}
