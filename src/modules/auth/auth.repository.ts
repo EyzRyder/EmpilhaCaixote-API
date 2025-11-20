4d584dc926610cb194f340de5efcebe9b840fff5
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-20 17:57:34 -0300

    fix(repository): expand type

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,9 +1,10 @@
 import { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import { profiles } from "../../db/schema";
 import { eq } from "drizzle-orm";
+import * as schema from "../../db/schema";
 
 export class AuthRepository {
-  constructor(private db: BetterSQLite3Database) {}
+  constructor(private db: BetterSQLite3Database<typeof schema>) {}
 
   async findByUsername(username: string) {
     return this.db.select().from(profiles).where(eq(profiles.username, username)).get();
