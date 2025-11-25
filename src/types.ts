3e75db1bffae34ebe38e2dd07dbe346555e11dc6
Gabriel Bessi <gabriel.bessi@grupoicr.com.br>
2025-11-25 16:25:09 -0300

    feat: add more types

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,19 @@
 import { InferSelectModel } from "drizzle-orm";
 import {
   profiles,
+  shopCoinPacks,
+  shopGemPacks,
   shopPowers,
   shopSkins,
   userEquippedItems,
+  userPowers,
+  userSkins,
 } from "./db/schema";
-import {  WebSocket } from "ws";
+import { WebSocket } from "ws";
 
 export type Player = {
   ws: WebSocket; // WebSocket connection
-} &Pick<User, "id"|"username">;
+} & Pick<User, "id" | "username">;
 
 export type Room = {
   id: string;
@@ -21,8 +25,8 @@ export type Room = {
   board: number[][]; // connect 4 6x7
   turn: number; // index do jogador (0 ou 1)
   gameStarted: boolean;
-  blockedColumns: number[]; 
-  timer?: NodeJS.Timeout;  
+  blockedColumns: number[];
+  timer?: NodeJS.Timeout;
   timeLeft: number;
 };
 
@@ -33,3 +37,11 @@ export type ShopSkins = InferSelectModel<typeof shopSkins>;
 export type UserEquippedItems = InferSelectModel<typeof userEquippedItems>;
 
 export type ShopPowers = InferSelectModel<typeof shopPowers>;
+
+export type ShopGemPacks = InferSelectModel<typeof shopGemPacks>;
+
+export type ShopCoinPacks = InferSelectModel<typeof shopCoinPacks>;
+
+export type UserPowers = InferSelectModel<typeof userPowers>;
+
+export type UserSkins = InferSelectModel<typeof userSkins>;
